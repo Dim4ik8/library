97ecedc99729cd111061bcbb7a3f818da219dd45
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-10-30 19:21:53 +0300

    Add url to image for every book

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 from bs4_tutorial import download_txt
 from bs4 import BeautifulSoup
 from bs4_tutorial import check_for_redirect
-
+from urllib.parse import urljoin
 
 def main():
+    main_url = "https://tululu.org/"
     book_url = "https://tululu.org/txt.php"
     book_start_url = 'https://tululu.org/b'
 
@@ -19,8 +20,14 @@ def main():
             soup = BeautifulSoup(response.text, 'lxml')
             title_tag = soup.find('h1')
             filename = title_tag.text.split('::')[0].strip()
+            image = soup.find('div', class_='bookimage').find('img')['src']
+            if image != '/images/nopic.gif':
+                image_url = urljoin(main_url, image)
 
-            download_txt(book_url, filename, params=params)
+                print(f'Название: {filename}' )
+                print(image_url)
+                print('='*120)
+            # download_txt(book_url, filename, params=params)
 
         except requests.TooManyRedirects:
             continue
