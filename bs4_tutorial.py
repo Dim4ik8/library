98b71f3679843328c668b009e130d6809d683865
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-10-30 11:42:47 +0300

    Add output title and author of book

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -1,18 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.franksonnenbergonline.com/blog/are-you-grateful/'
+url = 'https://tululu.org/b1/'
 response = requests.get(url)
 response.raise_for_status()
 
-
 soup = BeautifulSoup(response.text, 'lxml')
 
-title_tag = soup.find('main').find('header').find('h1')
-print(title_tag.text)
-
-image = soup.find('img', class_='attachment-post-image')['src']
-print(image)
-
-post_text = soup.find('div', class_='entry-content').text
-print(post_text)
+title_tag = soup.find('h1')
+title = title_tag.text.split('::')[0].strip()
+author = title_tag.text.split('::')[-1].strip()
+print(f'Название: {title}')
+print(f'Автор: {author}')
