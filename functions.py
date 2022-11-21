69a403722d175ddb5a8d323381b4cdc25f96756c
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-11-21 22:20:55 +0300

    Add try except for HTTP and conniction errors

--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
 import requests
 import os
-from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from pathlib import Path
 
@@ -25,46 +24,32 @@ def download_txt(url, filename, folder='books/', params=None):
         pass
 
 
-
 def download_image(url, filename, folder='images'):
     response = requests.get(url)
     response.raise_for_status()
     Path(folder).mkdir(exist_ok=True)
-    with open(f'{os.path.join(folder, filename)}.jpg', 'wb') as file:
+    with open(f'{os.path.join(folder, filename)}', 'wb') as file:
         file.write(response.content)
 
-def parse_book_page(soup):
 
+def parse_book_page(soup):
     title_tag = soup.find('h1')
-    book_info = {'title': title_tag.text.split('::')[0].strip(),
-                'author': title_tag.text.split('::')[-1].strip(),
-                'genres': [genre.text for genre in soup.find('span', class_='d_book').find_all('a')]
-                 }
+    book_info = {
+        'title': title_tag.text.split('::')[0].strip(),
+        'author': title_tag.text.split('::')[-1].strip(),
+        'genres': [genre.text for genre in soup.find('span', class_='d_book').find_all('a')]
+    }
 
     return book_info
 
+
 def main():
-    book_start_url = 'https://tululu.org/b'
-    response = requests.get(url)
-    response.raise_for_status()
+    url_book = 'http://tululu.org/txt.php?id=1'
+    print(download_txt(url_book, 'Алиби'))
 
+    print(download_txt(url_book, 'Али/би', folder='books/'))
 
-    # soup = BeautifulSoup(response.text, 'lxml')
-    for count in range(10):
-        url = book_start_url + str(count + 1) + '/'
-
-    # title_tag = soup.find('h1')
-    # title = title_tag.text.split('::')[0].strip()
-    # author = title_tag.text.split('::')[-1].strip()
-    # print(f'Название: {title}')
-    # print(f'Автор: {author}')
-    #
-    # url_book = 'http://tululu.org/txt.php?id=1'
-    # print(download_txt(url_book, 'Алиби'))
-    #
-    # print(download_txt(url_book, 'Али/би', folder='books/'))
-    #
-    # print(download_txt(url_book, 'Али\\би', folder='txt/'))
+    print(download_txt(url_book, 'Али\\би', folder='txt/'))
 
 
 if __name__ == '__main__':
--- parse_tululu.py
+++ parse_tululu.py
@@ -1,33 +1,47 @@
+import time
+from urllib.parse import urljoin
+
 import requests
 from functions import parse_book_page
 from bs4 import BeautifulSoup
-from functions import check_for_redirect
+from functions import check_for_redirect, download_image
 import argparse
 
 
 def main():
-
     parser = argparse.ArgumentParser(
         description='Парсим библиотеку'
     )
-    parser.add_argument('-s', '--start_id', help='С какой страницы качать', nargs='?', default='1', type=int)
-    parser.add_argument('-e', '--end_id', help='По какую страницу качать', nargs='?', default='10', type=int)
+    parser.add_argument(
+        '-s', '--start_id',
+        help='С какой страницы качать',
+        nargs='?',
+        default='1',
+        type=int
+    )
+    parser.add_argument(
+        '-e',
+        '--end_id',
+        help='По какую страницу качать',
+        nargs='?',
+        default='10',
+        type=int
+    )
     args = parser.parse_args()
 
     start = args.start_id
     end = args.end_id
 
     main_url = "https://tululu.org/"
-    book_url = "https://tululu.org/txt.php"
     book_start_url = 'https://tululu.org/b'
 
     for count in range(start, end):
 
-        url = f'{book_start_url}{str(count)}/'
-        response = requests.get(url)
-        response.raise_for_status()
-
         try:
+            url = f'{book_start_url}{str(count)}/'
+            response = requests.get(url)
+            response.raise_for_status()
+
             check_for_redirect(response)
             soup = BeautifulSoup(response.text, 'lxml')
 
@@ -35,28 +49,33 @@ def main():
             print('Заголовок: ', book_info['title'])
             print('Автор: ', book_info['author'])
             print('Жанр: ', book_info['genres'])
-            print('='*120)
+            print('=' * 120)
 
+            image = soup.find('div', class_='bookimage').find('img')['src']
+            image_url = urljoin(main_url, image)
+            image_title = image.split('/')[-1]
+            print(image_url)
+            download_image(image_url, image_title)
 
-            # title_tag = soup.find('h1')
-            # filename = title_tag.text.split('::')[0].strip()
-            # image = soup.find('div', class_='bookimage').find('img')['src']
-            # image_url = urljoin(main_url, image)
-            # image_title = image.split('/')[-1]
+            comments = soup.find_all('div', class_='texts')
+            com = [comment.find('span', class_='black') for comment in comments]
+            for text in com:
+                print(text.text)
 
-            # comments = soup.find_all('div', class_='texts')
-            # com = [comment.find('span', class_='black') for comment in comments]
-            # print(filename)
-            # for text in com:
-            #     print(text.text)
+            genres = [genre.text for genre in soup.find('span', class_='d_book').find_all('a')]
+            print(genres)
+            print('=' * 120)
 
-
-            # genres = [genre.text for genre in soup.find('span', class_='d_book').find_all('a')]
-            # print(genres)
-            # print('=' * 120)
-
+            time.sleep(5)
         except requests.TooManyRedirects:
             continue
+
+        except requests.exceptions.HTTPError:
+            print('Some errors on server.. Try again')
+
+        except requests.exceptions.ConnectionError:
+            print('Please check your internet connection')
+            time.sleep(10)
 
 
 if __name__ == '__main__':
