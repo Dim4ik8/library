9374ad08bd698c6a39477279f1afa342e1aa26cf
Дмитрий Жлобо <dmitryzlobo@MacBook-Pro-Dmitrij.local>
2023-01-04 13:38:04 +0300

    Download books from all pages

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import requests
 import os
 from pathvalidate import sanitize_filename
 from pathlib import Path
-
+from urllib.parse import urljoin
 
 def check_for_redirect(response):
     if response.history:
@@ -46,7 +46,27 @@ def parse_book_page(soup):
 
     return book
 
+def download_the_book(soup, link_to_book, url_with_text, params):
+    title_tag = soup.find('h1')
+    title, author = title_tag.text.split(' :: ')
+    comments = soup.find_all('div', class_='texts')
+    image = soup.find('div', class_='bookimage').find('img')['src']
+    image_url = urljoin(link_to_book, image)
+    image_title = image.split('/')[-1]
+    download_image(image_url, image_title)
+    download_txt(url_with_text, title.strip(), params=params)
 
+    book = {
+        'title': title.strip(),
+        'author': author.strip(),
+        'img_src': os.path.join('images', image_title),
+        'book_path': os.path.join('books', f'{title.strip()}.txt'),
+        'genres': [genre.text for genre in soup.find('span', class_='d_book').find_all('a')],
+        'comments': [comment.find('span', class_='black').text for comment in comments]
+    }
+
+
+    return book
 def main():
     url_book = 'http://tululu.org/txt.php?id=1'
     print(download_txt(url_book, 'Алиби'))
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,31 +1,62 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
-from functions import parse_book_page, check_for_redirect
+import logging
+import re
+from functions import check_for_redirect, download_the_book
 from urllib.parse import urljoin
+from pprint import pprint
 
-def get_url_with_book(soup):
-    urls_with_books = soup.find_all(class_='d_book')
-    for url in urls_with_books:
-        second_part_of_url = url.find('a')['href']
-        url_with_book = urljoin(base_url, second_part_of_url)
-        print(url_with_book)
+logger = logging.getLogger()
+def get_links_to_books(soup):
+    books = soup.find_all(class_='d_book')
+    links_to_books = [urljoin(base_url, url.find('a')['href']) for url in books]
+    return links_to_books
 
 base_url = 'https://tululu.org'
-url_with_fantasy = 'https://tululu.org/l55/'
+url_to_fantasy_books = 'https://tululu.org/l55/'
+url_with_text = 'https://tululu.org/txt.php'
 
-response = requests.get(url_with_fantasy)
+response = requests.get(url_to_fantasy_books)
 response.raise_for_status()
 
 soup = BeautifulSoup(response.text, 'lxml')
+second_part_of_url = soup.find(class_='d_book').find('a')['href']
+url_to_first_book = urljoin(base_url, second_part_of_url)
 
-# second_part_of_url = soup.find(class_='d_book').find('a')['href']
+count_of_pages = 4
+books = []
 
-# url_with_first_book = urljoin(base_url, second_part_of_url)
-
-count = 10
-for page in range(1, count):
-    url_to_each_page = urljoin(url_with_fantasy, str(page))
+for page in range(1, count_of_pages+1):
+    url_to_each_page = urljoin(url_to_fantasy_books, str(page))
     response = requests.get(url_to_each_page)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    get_url_with_book(soup)
+    links_to_books = get_links_to_books(soup)
+    for link in links_to_books:
+        try:
+            id = re.findall('\d+', link)[0]
+            params = {'id': id}
+            response = requests.get(link)
+            response.raise_for_status()
+
+            check_for_redirect(response)
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            book = download_the_book(soup, link, url_with_text, params)
+
+            books.append(book)
+
+        except requests.TooManyRedirects:
+            logger.warning(f'There is no data for one book ..')
+            continue
+
+        except requests.exceptions.HTTPError:
+            logger.warning('Some errors on server.. Try again')
+            continue
+
+        except requests.exceptions.ConnectionError:
+
+            logger.warning('Please check your internet connection')
+            time.sleep(10)
