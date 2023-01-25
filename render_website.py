2ff1d4124942e3a57623fa1a5937d49e11647686
DmitryZhlobo <dmitry.zhlobo@yandex.ru>
2023-01-25 20:07:09 +0300

    Add one more chunked

--- render_website.py
+++ render_website.py
@@ -26,24 +26,34 @@ json_path = find('books.json', pathlib.Path.cwd())
 with open(json_path, 'r') as file:
     books = json.load(file)
 
-books_for_2_col = list(chunked(books, 2))
+by_10_books_on_page = list(chunked(books, 10))
+
 
 os.makedirs('pages', exist_ok=True)
-
-by_10_books_on_page = list(chunked(books_for_2_col, 5))
 
 
 for count, books in enumerate(by_10_books_on_page):
     num_of_page = count + 1
+    books_for_two_columns = list(chunked(books, 2))
+
     page_title = f'index{num_of_page}.html'
     template = env.get_template('template.html')
     rendered_page = template.render(
-        books=books,
+        books=books_for_two_columns,
         total_pages=len(by_10_books_on_page),
         current_page=count + 1,
     )
+
+    for two_books in books_for_two_columns:
+        print(two_books[0]['img_src'])
+        print('='*50)
+
     with open(f'pages/{page_title}', 'w', encoding="utf8") as file:
         file.write(rendered_page)
+
+
+
+
     # def on_reload():
     #     template = env.get_template('template.html')
     #     rendered_page = template.render(
