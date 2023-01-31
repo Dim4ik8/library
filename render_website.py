1a2526f08c22c6a0205eccef3d879b12c57af2dc
DmitryZhlobo <dmitry.zhlobo@yandex.ru>
2023-01-31 18:26:56 +0300

    Add if __name__ == '__main__' to render_website.py

--- render_website.py
+++ render_website.py
@@ -18,39 +18,38 @@ def find(name, path):
         if name in files:
             return os.path.join(root, name)
 
-
-json_path = find('books.json', pathlib.Path.cwd())
-
-with open(json_path, 'r', encoding='utf-8') as file:
-    books_descriptions = json.load(file)
-
-book_cards_per_page = 10
-book_cards_in_column = 2
-books_on_page = list(chunked(books_descriptions, book_cards_per_page))
-
-os.makedirs('pages', exist_ok=True)
-
-
 def on_reload():
     for page, books in enumerate(books_on_page, start=1):
-        books_for_columns = list(chunked(books, book_cards_in_column))
+        books_in_column = list(chunked(books, book_cards_in_column))
         page_title = f'index{page}.html'
 
         template = env.get_template('template.html')
         rendered_page = template.render(
-            books=books_for_columns,
+            books=books_in_column,
             total_pages=len(books_on_page),
             current_page=page,
         )
 
-        with open(f'pages/{page_title}', 'w', encoding="utf8") as file:
+        with open(f'pages/{page_title}', 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
-    print('Site rebuilt')
+def main():
+    json_path = find('books.json', pathlib.Path.cwd())
 
+    with open(json_path, 'r', encoding='utf-8') as file:
+        books_descriptions = json.load(file)
 
-# on_reload()
-#
-# server = Server()
-# server.watch('template.html', on_reload)
-# server.serve(root='.')
+    book_cards_per_page = 10
+    book_cards_in_column = 2
+    books_on_page = list(chunked(books_descriptions, book_cards_per_page))
+
+    os.makedirs('pages', exist_ok=True)
+
+    on_reload()
+
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
+
+if __name__ == '__main__':
+    main()
