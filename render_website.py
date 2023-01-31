e77307025221c8f8dcd3dbb74162bc3fa5de0bbe
DmitryZhlobo <dmitry.zhlobo@yandex.ru>
2023-01-31 21:34:13 +0300

    Update README.md

--- render_website.py
+++ render_website.py
@@ -18,9 +18,9 @@ def find(name, path):
         if name in files:
             return os.path.join(root, name)
 
-def on_reload(books_on_page, book_cards_in_column):
+def on_reload(books_on_page, book_cards_in_row):
     for page, books in enumerate(books_on_page, start=1):
-        books_in_column = list(chunked(books, book_cards_in_column))
+        books_in_column = list(chunked(books, book_cards_in_row))
         page_title = f'index{page}.html'
 
         template = env.get_template('template.html')
@@ -40,15 +40,15 @@ def main():
         books_descriptions = json.load(file)
 
     book_cards_per_page = 10
-    book_cards_in_column = 2
+    book_cards_in_row = 2
     books_on_page = list(chunked(books_descriptions, book_cards_per_page))
 
     os.makedirs('pages', exist_ok=True)
 
-    on_reload(books_on_page, book_cards_in_column)
+    on_reload(books_on_page, book_cards_in_row)
 
     server = Server()
-    server.watch('template.html', on_reload(books_on_page, book_cards_in_column))
+    server.watch('template.html', on_reload(books_on_page, book_cards_in_row))
     server.serve(root='.')
 
 if __name__ == '__main__':
