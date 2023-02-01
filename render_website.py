2331929010360cd8eb14859145bbade3331bd5de
Dmitry <you@example.com>
2023-02-01 16:03:30 +0300

    Delete the second chunked

--- render_website.py
+++ render_website.py
@@ -18,14 +18,14 @@ def find(name, path):
         if name in files:
             return os.path.join(root, name)
 
-def on_reload(books_on_page, book_cards_in_row):
+def on_reload(books_on_page):
     for page, books in enumerate(books_on_page, start=1):
-        books_in_column = list(chunked(books, book_cards_in_row))
+        # books_in_column = list(chunked(books, book_cards_in_row))
         page_title = f'index{page}.html'
 
         template = env.get_template('template.html')
         rendered_page = template.render(
-            books=books_in_column,
+            books=books,
             total_pages=len(books_on_page),
             current_page=page,
         )
@@ -40,15 +40,15 @@ def main():
         books_descriptions = json.load(file)
 
     book_cards_per_page = 10
-    book_cards_in_row = 2
+    # book_cards_in_row = 2
     books_on_page = list(chunked(books_descriptions, book_cards_per_page))
 
     os.makedirs('pages', exist_ok=True)
 
-    on_reload(books_on_page, book_cards_in_row)
+    on_reload(books_on_page)
 
     server = Server()
-    server.watch('template.html', on_reload(books_on_page, book_cards_in_row))
+    server.watch('template.html', on_reload(books_on_page))
     server.serve(root='.')
 
 if __name__ == '__main__':
